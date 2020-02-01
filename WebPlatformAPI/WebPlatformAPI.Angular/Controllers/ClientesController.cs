9dafcc0a35443f77377e50fc73b2a5c7e5a91dba
Daniel <dannyb1392@gmail.com>
2020-02-01 08:51:50 -0400

    CrudClienteController
    Completing ClienteController'S Crud
    CrudClienteController
    
    Completing ClienteController'S Crud
    Adding Region

--- WebPlatformAPI/WebPlatformAPI.Angular/Controllers/ClientesController.cs
+++ WebPlatformAPI/WebPlatformAPI.Angular/Controllers/ClientesController.cs
@@ -1,28 +1,96 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using WebPlatformAPI.Angular.Context;
 using WebPlatformAPI.Angular.Models;
 
 namespace WebPlatformAPI.Angular.Controllers
-{   
+{
     [Route("[controller]")]
     [ApiController]
     public class ClientesController : ControllerBase
     {
         private readonly ApplicationDbContext context;
 
+        #region Ctor
         public ClientesController(ApplicationDbContext context)
         {
             this.context = context;
-        }
+        } 
+        #endregion
 
+        #region GetAll
+        //Devuelve Todos los Clientes
         [HttpGet()]
-        public ActionResult<IEnumerable<Cliente>> Get()
+        public async Task<ActionResult<IEnumerable<Cliente>>> Get()
         {
-            return context.clientes.ToList();
+            var clietes = await context.clientes.ToListAsync();
 
-            //return Ok(cliente);
+            return Ok(clietes);
         }
+        #endregion
+
+        #region GetOne
+        //Devueve Un solo Cliente
+        [HttpGet("{id}", Name = "ObtenerCliente")]
+        public async Task<ActionResult<Cliente>> Get(int id)
+        {
+            var cliente = await context.clientes.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return cliente;
+        }
+        #endregion
+
+        #region Post
+
+        // Crea Cliente
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Cliente cliente)
+        {
+            context.Add(cliente);
+            await context.SaveChangesAsync();
+            return new CreatedAtRouteResult("ObtenerCliente", new { id = cliente.Id }, cliente);
+        }
+        #endregion
+
+        #region Put 
+        // PUT Actualiza un Clliente
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] Cliente cliente)
+        {
+            cliente.Id = id;
+            context.Entry(cliente).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        #endregion
+
+        #region Delete
+
+        // DELETE Borra un Cliente
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Cliente>> Delete(int id)
+        {
+            var ClienteId = await context.clientes.Select(x => x.Id).FirstOrDefaultAsync(x => x == id);
+
+            if (ClienteId == default(int))
+            {
+                return NotFound();
+            }
+
+            context.Remove(new Cliente { Id = ClienteId });
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        #endregion
     }
 }
